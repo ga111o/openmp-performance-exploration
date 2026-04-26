1cafe61a23477864e763dcfdfae03704c73540b2
ga111o <ga111o@proton.me>
2026-04-27 00:26:31 +0900

    improve timing functions

--- main.c
+++ main.c
@@ -3,10 +3,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static inline uint64_t rdtsc(void) {
+#define WARM_TRIALS 64
+#define WORK_TRIALS 5
+
+static inline uint64_t rdtsc_s(void) {
     uint32_t lo;
     uint32_t hi;
-    __asm__ __volatile__("rdtsc" : "=a"(lo), "=d"(hi));
+    __asm__ __volatile__(
+        "lfence\n\t"
+        "rdtsc\n\t"
+        "lfence"
+        : "=a"(lo), "=d"(hi)
+        :
+        : "memory"
+    );
     return ((uint64_t)hi << 32) | lo;
 }
 
@@ -26,42 +36,60 @@ static inline uint64_t fixed_asm_op(uint64_t x) {
 }
 
 int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "usage: %s <iterations>\n", argv[0]);
+        return 1;
+    }
+
     uint64_t iterations = strtoull(argv[1], NULL, 10);
+    int nthreads = omp_get_max_threads();
     uint64_t acc = 0;
 
-    int nthreads = omp_get_max_threads();
-    uint64_t *tsc_create = (uint64_t *)calloc((size_t)nthreads, sizeof(uint64_t));
-    uint64_t *tsc_destroy = (uint64_t *)calloc((size_t)nthreads, sizeof(uint64_t));
+    uint64_t t0 = rdtsc_s();
+    #pragma omp parallel num_threads(nthreads)
+    { (void)omp_get_thread_num(); }
+    uint64_t t1 = rdtsc_s();
+    uint64_t pool_create = t1 - t0;
 
-    uint64_t tsc_before = rdtsc();
+    uint64_t fork_join_min = UINT64_MAX;
+    for (int i = 0; i < WARM_TRIALS; ++i) {
+        uint64_t a = rdtsc_s();
+        #pragma omp parallel num_threads(nthreads)
+        { (void)omp_get_thread_num(); }
+        uint64_t b = rdtsc_s();
+        uint64_t d = b - a;
+        if (d < fork_join_min) fork_join_min = d;
+    }
 
-    #pragma omp parallel num_threads(nthreads) reduction(^:acc)
-    {
-        int tid = omp_get_thread_num();
-        tsc_create[tid] = rdtsc();
-
-        #pragma omp for schedule(static)
-        for (uint64_t i = 0; i < iterations; ++i) {
-            acc ^= fixed_asm_op(i);
+    uint64_t serial_min = UINT64_MAX;
+    for (int i = 0; i < WORK_TRIALS; ++i) {
+        uint64_t a = rdtsc_s();
+        for (uint64_t k = 0; k < iterations; ++k) {
+            acc ^= fixed_asm_op(k);
         }
-
-        tsc_destroy[tid] = rdtsc();
+        uint64_t b = rdtsc_s();
+        uint64_t d = b - a;
+        if (d < serial_min) serial_min = d;
     }
 
-    uint64_t tsc_after = rdtsc();
-
-    printf("acc=%llu\n", (unsigned long long)acc);
-    printf("threads=%d\n", nthreads);
-    for (int t = 0; t < nthreads; ++t) {
-        uint64_t create_cycles = tsc_create[t] - tsc_before;
-        uint64_t destroy_cycles = tsc_after - tsc_destroy[t];
-        printf("tid=%d create=%llu destroy=%llu\n",
-               t,
-               (unsigned long long)create_cycles,
-               (unsigned long long)destroy_cycles);
+    uint64_t parallel_min = UINT64_MAX;
+    for (int i = 0; i < WORK_TRIALS; ++i) {
+        uint64_t a = rdtsc_s();
+        #pragma omp parallel for num_threads(nthreads) reduction(^:acc) schedule(static)
+        for (uint64_t k = 0; k < iterations; ++k) {
+            acc ^= fixed_asm_op(k);
+        }
+        uint64_t b = rdtsc_s();
+        uint64_t d = b - a;
+        if (d < parallel_min) parallel_min = d;
     }
 
-    free(tsc_create);
-    free(tsc_destroy);
+    long long pure_parallel = (long long)parallel_min - (long long)fork_join_min;
+    double speedup_total = (double)serial_min / (double)parallel_min;
+    double speedup_pure = pure_parallel > 0
+        ? (double)serial_min / (double)pure_parallel
+        : 0.0;
+
+    printf("threads=%d iterations=%llu\n", nthreads, (unsigned long long)iterations);
     return 0;
 }
